9f92a404ea0a27bb54adbea5cc0fed9857c61031
khushilms <khushilms.cs19@bmsce.ac.in>
2023-01-15 17:21:50 +0530

    chore: finish caesar cipher function

---
+++ QuestionNine/questionNine.js
@@ -0,0 +1,19 @@
+const caesarCipher = (plainText) => {
+  let cipherText = "";
+  const plainTextLen = plainText.length;
+  for (let i = 0; i < plainTextLen; i++) {
+    if (plainText.charAt(i) === "z") {
+      cipherText += "a"
+      continue;
+    } else if (plainText.charAt(i) === "Z") {
+      cipherText += "A"
+      continue;
+    }
+    cipherText += String.fromCharCode(plainText.charCodeAt(i) + 1);
+  }
+  return cipherText;
+}
+
+module.exports = {
+  caesarCipher,
+}
---
+++ QuestionNine/questionNine.test.js
@@ -0,0 +1,8 @@
+const { caesarCipher } = require("./questionNine");
+
+test("check caesar cipher", () => {
+  expect(caesarCipher("codeacademy")).toBe("dpefbdbefnz");
+})
+test("check caesar cipher", () => {
+  expect(caesarCipher("lazyinterns")).toBe("mbazjoufsot");
+})
