81a346c526332bf07e5ea3476197f23f9ea9bdb5
khushilms <khushilms.cs19@bmsce.ac.in>
2023-01-15 16:54:44 +0530

    chore: finish count vowel function

---
+++ QuestionEight/questionEight.js
@@ -0,0 +1,19 @@
+
+const isVowel = (char) => {
+  const pattern = /[aeiouAEIOU]/;
+  return pattern.test(char);
+}
+
+const countVowels = (str) => {
+  let count = 0;
+  str.split("").forEach((char) => {
+    if (isVowel(char)) {
+      count += 1;
+    }
+  });
+  return count;
+}
+
+module.exports = {
+  countVowels,
+}
